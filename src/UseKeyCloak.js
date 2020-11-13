d3172d71774321b4df308d9430a9ccc5be583d6b
Tomas Pinto <tomas.pinto@spark.net>
2020-11-13 09:42:44 +0100

    useKeycloak now return Keycloak object

--- src/UseKeyCloak.js
+++ src/UseKeyCloak.js
@@ -4,22 +4,24 @@ import Keycloak from 'keycloak-js';
 
 function useKeyCloak() {
     const [authenticated, setAuthenticated] = useState(false);
-    const keycloak = Keycloak('/keycloak.json');
+    const [keycloak, setKeycloak] = useState(null);
 
     useEffect(() => {
-
-
         var initSetting = { onLoad: 'login-required' }
+        var keycloak = Keycloak('/keycloak.json');
 
         keycloak.init(initSetting)
             .then(authenticated => {
                 console.log("init - authenticated: ", authenticated)
                 setAuthenticated(authenticated);
+                setKeycloak(keycloak)
             });
         console.log("authenticated: ", authenticated)
     }, []);
 
-    return authenticated
+    console.log("stocazzo: ", keycloak)
+
+    return [authenticated, keycloak]
 }
 
 export default useKeyCloak
