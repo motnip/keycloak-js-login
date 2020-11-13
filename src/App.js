53a6fdc5ec2c633a32a524d27c5359a8a679462d
Tomas Pinto <tomas.pinto@spark.net>
2020-11-13 09:43:26 +0100

    Added new route to secure2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Welcome from './Welcome';
 import Secured from './Secured';
+import Secured2 from './Secured2';
 import NoLogin from './NoLogin';
 import './App.css';
 
@@ -13,9 +14,11 @@ function App() {
           <ul>
             <li><Link to="/welcome">public component</Link></li>
             <li><Link to="/secured">secured component</Link></li>
+            <li><Link to="/secured2">secured2 component</Link></li>
           </ul>
           <Route exact path="/welcome" component={Welcome} />
           <Route path="/secured" component={Secured} />
+          <Route path="/secured2" component={Secured2} />
           <Route path="/nologin" component={NoLogin} />
         </div>
       </BrowserRouter>
