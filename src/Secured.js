972780a4f0ad7bb4aaabe2421aec0d78ca2bcb4d
Tomas Pinto <tomas.pinto@spark.net>
2020-11-15 17:11:14 +0100

    Added login link and clean code

--- src/Secured.js
+++ src/Secured.js
@@ -6,20 +6,18 @@ import React, { useEffect } from 'react';
 export default function Secured() {
   const [authenticated, keycloak] = useKeyCloak();
 
-  useEffect(() => {
-    console.log("state keycloak", keycloak)
-    console.log("state authenticated", authenticated)
-  }, []);
   return (
 
     <div>
-      {keycloak && authenticated && (<div>
+      {keycloak && keycloak.authenticated && (<div>
         <div>loggin succeed</div>
-        <div> <UserInfo keycloak={keycloak} authenticated={authenticated} /></div>
+        <div> <UserInfo keycloak={keycloak}/></div>
         <div><Logout keycloak={keycloak} /></div>
       </div>
       )}
-      {!authenticated && <div>loggin falied</div>}
+      {keycloak && !keycloak.authenticated &&
+        (<div><a onClick={() => keycloak.login()}>Login</a></div>)
+      }
     </div>
 
   )
